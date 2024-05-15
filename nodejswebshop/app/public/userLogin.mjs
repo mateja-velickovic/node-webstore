89cfdbe290e40cbfa6b74bb1ed3c1f07b6d5211d
Mateja Velickovic <mateja.velickovic@eduvaud.ch>
2024-05-15 11:09:59 +0200

    :large_blue_diamond: feat(app): App update

--- nodejswebshop/app/public/userLogin.mjs
+++ nodejswebshop/app/public/userLogin.mjs
@@ -4,14 +4,13 @@ document.addEventListener('DOMContentLoaded', function() {
     form.addEventListener('submit', function(event) {
         event.preventDefault(); // Empêche la soumission du formulaire par défaut
 
-        // Récupérer les valeurs des champs du formulaire
+        // Données entrées par l'utilisateur 
         const username = document.getElementById('username').value;
         const password = document.getElementById('password').value;
 
         fetch("mock-users.json")
             .then(response => response.json())
             .then(users => {
-                // Vérifier si le nom d'utilisateur existe dans les données
                 const user = users.find(user => user.name === username);
 
                 if (!user ) {
@@ -24,12 +23,10 @@ document.addEventListener('DOMContentLoaded', function() {
                     return;
                 }
 
-                // Si les informations d'identification sont correctes
                 console.log('Connexion réussie');
-                // Rediriger vers une autre page ou effectuer d'autres actions nécessaires
             })
             .catch(error => {
-                console.error('Erreur lors du chargement des données utilisateur:', error);
+                console.error('Erreur : ', error);
             });
     });
 });
